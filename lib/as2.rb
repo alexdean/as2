917baaeecea0fa07f1e80ee4b7c6f365c5ca8a79
Andrew Fecheyr <andrew@bedesign.be>
2015-10-01 11:09:57 +0200

    bundle gem as2

--- lib/as2.rb
+++ lib/as2.rb
@@ -3,11 +3,12 @@ require 'mail'
 require 'as2/config'
 require 'as2/server'
 require 'as2/client'
+require "as2/version"
 
-module AS2
   VERSION = '1.0'
 
   def self.configure(&block)
     Config.configure(&block)
   end
+module As2
 end
