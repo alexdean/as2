ea57501fac3a0fcab0a26bda4f2853b2680f422c
Andrew Fecheyr <andrew@bedesign.be>
2015-11-09 14:26:17 +0100

    Cleaner test code

--- test/message_test.rb
+++ test/message_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class MessageTest < Minitest::Test
-  def test_that_message_can_be_decoded
-    server_key = OpenSSL::PKey.read File.read('test/certificates/server.key')
-    server_crt = OpenSSL::X509::Certificate.new File.read('test/certificates/server.crt')
-    client_crt = OpenSSL::X509::Certificate.new File.read('test/certificates/client.crt')
+  def private_key(path)
+    OpenSSL::PKey.read File.read(path)
+  end
+
+  def public_key(path)
+    OpenSSL::X509::Certificate.new File.read(path)
+  end
+
+  def test_base64_encoded_messages
+    server_key = private_key('test/certificates/server.key')
+    server_crt = public_key('test/certificates/server.crt')
+    client_crt = public_key('test/certificates/client.crt')
     message = As2::Message.new(File.read('test/fixtures/hello_message'), server_key, server_crt)
     assert message.valid_signature?(client_crt), "Invalid signature"
     assert_equal "Hello World\n", message.attachment.body.decoded
