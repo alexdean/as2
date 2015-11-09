809948a828a7d6fb53cc1955d005ecf7ea8883be
Andrew Fecheyr <andrew@bedesign.be>
2015-11-09 14:03:23 +0100

    Add instructions for running server and client examples

--- examples/server.rb
+++ examples/server.rb
@@ -2,15 +2,15 @@ require 'as2'
 require 'rack'
 
 As2.configure do |conf|
-  conf.name = 'MyName'
+  conf.name = 'MyServer'
   conf.url = 'http://localhost:3000/as2'
-  conf.certificate = 'server.crt'
-  conf.pkey = 'server.key'
+  conf.certificate = 'test/certificates/server.crt'
+  conf.pkey = 'test/certificates/server.key'
   conf.domain = 'mydomain.com'
   conf.add_partner do |partner|
-    partner.name = 'mycompanyAS2'
+    partner.name = 'MyClient'
     partner.url = 'http://localhost:8080/as2/HttpReceiver'
-    partner.certificate = 'partner.crt'
+    partner.certificate = 'test/certificates/client.crt'
   end
 end
 
@@ -29,4 +29,5 @@ builder = Rack::Builder.new do
   end
 end
 
+puts "As2 version: #{As2::VERSION}"
 Rack::Handler::Thin.run builder, Port: 3000
