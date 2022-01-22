432eb0a47f19174fbc25c7229b5cf157bbb41e8e
Alex Dean <alexdean@transfix.io>
2022-01-22 09:55:26 -0600

    fix kwargs for ruby 3

--- test/client/result_test.rb
+++ test/client/result_test.rb
@@ -16,25 +16,25 @@ describe As2::Client::Result do
 
   describe '#success' do
     it 'is true when everything looks good' do
-      subject = As2::Client::Result.new(@attributes)
+      subject = As2::Client::Result.new(**@attributes)
       assert subject.success
     end
 
     it 'is true on disposition processed/warning' do
       @attributes[:disposition] = 'automatic-action/MDN-sent-automatically; processed/Warning: authentication-failed, processing continued'
-      subject = As2::Client::Result.new(@attributes)
+      subject = As2::Client::Result.new(**@attributes)
       assert subject.success
     end
 
     it 'is false on disposition processed/error' do
       @attributes[:disposition] = 'automatic-action/MDN-sent-automatically; processed/error: authentication-failed'
-      subject = As2::Client::Result.new(@attributes)
+      subject = As2::Client::Result.new(**@attributes)
       assert !subject.success
     end
 
     it 'is false if there are signature verification issues' do
       @attributes[:signature_verification_error] = 'missing certificate'
-      subject = As2::Client::Result.new(@attributes)
+      subject = As2::Client::Result.new(**@attributes)
       assert !subject.success
     end
   end
@@ -42,13 +42,13 @@ describe As2::Client::Result do
   describe '#signature_verified' do
     it 'is true if signature_verification_error is nil' do
       @attributes[:signature_verification_error] = nil
-      subject = As2::Client::Result.new(@attributes)
+      subject = As2::Client::Result.new(**@attributes)
       assert subject.signature_verified
     end
 
     it 'is false if signature_verification_error is present' do
       @attributes[:signature_verification_error] = 'missing certificate'
-      subject = As2::Client::Result.new(@attributes)
+      subject = As2::Client::Result.new(**@attributes)
       assert !subject.signature_verified
     end
   end
