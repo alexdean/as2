f1f78e4520dc2dc28759a86933e7bdf2edc7598a
Alex Dean <alexdean@transfix.io>
2022-08-17 10:53:59 -0500

    enhancements to DigestSelector
    useful when/if we want to try to generate a MIC using algo specified by partner

--- lib/as2/digest_selector.rb
+++ lib/as2/digest_selector.rb
@@ -10,15 +10,31 @@ module As2
       'md5' => OpenSSL::Digest::MD5
     }
 
+    # @return [Array] all the codes we understand
     def self.valid_codes
       @map.keys
     end
 
-    def self.for_code(code)
-      # we may receive 'sha256', 'sha-256', or 'SHA256'.
-      normalized = code.strip.downcase.gsub(/[^a-z0-9]/, '')
+    # @param [String] code an algorithm identifier like 'sha256' or 'md5'
+    # @return [boolean] do we recognize this code?
+    def self.valid_code?(code)
+      normalized = normalize(code)
+      valid_codes.include?(normalized)
+    end
 
+    # @param [String] code an algorithm identifier like 'sha256' or 'md5'
+    # @return [Class] an OpenSSL::Digest class implementing the requested algorithm
+    #   returns OpenSSL::Digest::SHA1 if the requested code is not recognized
+    def self.for_code(code)
+      normalized = normalize(code)
       @map[normalized] || OpenSSL::Digest::SHA1
+    end
+
+    private
+
+    def self.normalize(code)
+      # we may receive 'sha256', 'sha-256', or 'SHA256'.
+      code.strip.downcase.gsub(/[^a-z0-9]/, '')
     end
   end
 end
--- test/digest_selector_test.rb
+++ test/digest_selector_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 describe As2::DigestSelector do
+  describe '.valid_codes' do
+    it 'returns an array of recognized codes' do
+      assert_equal Array, As2::DigestSelector.valid_codes.class
+    end
+  end
+
+  describe '.valid_code?' do
+    it 'indicates if the given code is understood' do
+      assert As2::DigestSelector.valid_code?('sha256')
+      assert As2::DigestSelector.valid_code?('sha-256')
+      assert !As2::DigestSelector.valid_code?('sha-1000')
+    end
+  end
+
   describe '.for_code' do
     it 'can build a base64digest for all supported algorithm codes' do
       As2::DigestSelector.valid_codes.each do |code|
