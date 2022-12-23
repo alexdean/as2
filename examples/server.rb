033bafbf1f48fc87cae1380ca95ba09f7191b15e
Alex Dean <alexdean@transfix.io>
2022-12-23 10:10:36 -0600

    Revert "add updated example server which saves files it receives"
    This reverts commit 2357c387ab310256ea28d5ca59cf7fe777916cce.
    
    Will move "enhancements to server example" to a separate PR.

--- examples/server.rb
+++ examples/server.rb
@@ -1,67 +1,24 @@
-# test server receives files & saves them to the local filesystem
-#
-# `bundle exec ruby examples/server.rb`
-
 require 'as2'
 require 'rack'
-require 'pathname'
-
-this_dir = Pathname.new(File.expand_path('..', __FILE__))
-root_dir = this_dir.join('..')
 
 As2.configure do |conf|
-  conf.name = 'SERVER'
+  conf.name = 'MyServer'
   conf.url = 'http://localhost:3000/as2'
   conf.certificate = 'test/certificates/server.crt'
   conf.pkey = 'test/certificates/server.key'
-  conf.domain = 'localhost'
+  conf.domain = 'mydomain.com'
   conf.add_partner do |partner|
-    partner.name = 'CLIENT'
+    partner.name = 'MyClient'
     partner.url = 'http://localhost:8080/as2/HttpReceiver'
     partner.certificate = 'test/certificates/client.crt'
   end
 end
 
-handler = Proc.new do |env|
-  transmission_id = "#{Time.now.strftime('%Y%m%d_%H%M%S_%L')}_#{SecureRandom.hex(6)}"
-
-  server_info = As2::Config.server_info
-
-  partner_name = env['HTTP_AS2_FROM']
-  partner = As2::Config.partners[partner_name]
-
-  raw_request_body = env['rack.input'].read
-  # env['rack.input'].rewind
-  # request = Rack::Request.new(env)
-
-  message = As2::Message.new(raw_request_body, server_info.pkey, server_info.certificate)
-
-  partner_dir = root_dir.join('tmp/inbox/', partner_name, "#{transmission_id}_#{message.attachment.filename}")
-  if !File.exist?(partner_dir)
-    Dir.mkdir(partner_dir)
-  end
-
-  filename = message.attachment.filename
-  if filename.empty?
-    filename = "#{transmission_id}_content"
-  end
-  basename = partner_dir.join(filename).to_s
-
-  encrypted_filename = "#{basename}.pkcs7"
-  File.open(encrypted_filename, 'wb') { |f| f.write(raw_request_body) }
-  decrypted_filename = "#{basename}.mime"
-  File.open(decrypted_filename, 'wb') { |f| f.write(message.decrypted_message) }
-  File.open(basename, 'wb') { |f| f.write(message.attachment.body) }
-
-  prefix_length = root_dir.to_s.length + 1
-  puts "#{Time.now.strftime('%F %T')}: received transmission #{transmission_id}"
-  puts "     #{encrypted_filename[prefix_length..]}"
-  puts "     #{decrypted_filename[prefix_length..]}"
-  puts "     #{basename[prefix_length..]}"
-  puts "     valid_signature?: #{message.valid_signature?(partner.certificate)}"
-
-  server = As2::Server.new(server_info: server_info, partner: partner)
-  server.send_mdn(env, message.mic, message.mic_algorithm)
+handler = As2::Server.new do |filename, body|
+  puts "SUCCESSFUL DOWNLOAD"
+  puts "FILENAME: #{filename}"
+  puts
+  puts body
 end
 
 builder = Rack::Builder.new do
@@ -71,5 +28,5 @@ builder = Rack::Builder.new do
   end
 end
 
-puts "ruby-as2 version: #{As2::VERSION}"
-Rack::Handler::Thin.run builder, Port: 3002
+puts "As2 version: #{As2::VERSION}"
+Rack::Handler::Thin.run builder, Port: 3000
